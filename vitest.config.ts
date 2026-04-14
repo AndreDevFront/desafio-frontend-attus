04f1080071721d1ed3f780babb4faed62614efde
AndreDevFront <124165712+AndreDevFront@users.noreply.github.com>
2026-04-14 19:09:50 -0300

    fix(tests): adiciona plugin Angular no vitest.config para corrigir ProxyZone

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config';
+import angular from '@analogjs/vitest-angular/plugin';
 
 export default defineConfig({
+  plugins: [angular()],
   test: {
     globals: true,
     environment: 'jsdom',
